db0e566b9e2dc12ea39e922436dc8185812f73e1
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2019-05-24 18:19:42 +0300

    feat(app): use app-component

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import CssBaseLine from '@material-ui/core/CssBaseline';
+import App from './app-component/app.component';
 
 const Root: React.FC = () => {
   return (
     <React.Fragment>
       <CssBaseLine/>
+      <App/>
     </React.Fragment>
   );
 };

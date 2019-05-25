9875f9dc3daa366bad303a7b3affced5228a1ca4
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2019-05-25 17:01:54 +0300

    feat(app-component): implement pages routing

--- src/app/app-component/app.component.tsx
+++ src/app/app-component/app.component.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from '../shared/header.component';
+import Customers from '../customers/customers.component';
+import Products from '../products/products.component';
+import Invoices from '../invoices/invoices.component';
+import NotFound from '../not-found/not-found.component';
 
 import { NavMenuItem } from '../core/interfaces/nav-menu-item';
 
@@ -20,10 +24,20 @@ const App: React.FC = () => {
       link: '/invoices',
     },
   ];
+  
   return (
     <Router>
       <div className="wrapper">
         <Header navMenuList={navMenuList}/>
+        <main className='main'>
+          <Switch>
+            <Redirect exact from='/' to='/invoices'/>
+            <Route path="/customers" component={Customers}/>
+            <Route path="/products" component={Products}/>
+            <Route path="/invoices" component={Invoices}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </main>
       </div>
     </Router>
   );

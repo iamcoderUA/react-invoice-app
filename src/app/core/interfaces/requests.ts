17be246ae44fba1e508f148cb90682d30b389c24
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2019-06-07 15:20:48 +0300

    feat(core:interfaces): add requests interface

---
+++ src/app/core/interfaces/requests.ts
@@ -0,0 +1,17 @@
+import { Observable } from "rxjs";
+import { AjaxResponse } from "rxjs/ajax";
+
+import { Customer } from './customer';
+
+
+interface RequestPayload {
+  errors?: string,
+}
+
+export interface RequestPayloadCustomers extends RequestPayload {
+  data?: Customer[],
+}
+
+export interface RequestServiceCustomers {
+  getCustomers(payload?: RequestPayloadCustomers): Observable<AjaxResponse>;
+}

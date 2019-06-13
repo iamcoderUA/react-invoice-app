8d0fa781bcb1b1418cf129f0147094321954f775
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2019-06-13 15:23:15 +0300

    feat(root): provide store to the app-component

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+
 import CssBaseLine from '@material-ui/core/CssBaseline';
-import App from './app-component/app.component';
+
+import App from './components/app.component';
+import store from './store/store';
 
 const Root: React.FC = () => {
   return (
     <React.Fragment>
       <CssBaseLine/>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </React.Fragment>
   );
 };
